f1e69599691efcbaa8e47aa8056b4129edb9dd08
Rafael Borduchi <rafa_borduchi@hotmail.com>
2020-05-23 20:56:04 -0300

    Inciada tela do mapa

---
+++ src/pages/Main.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+export default function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -21.1223367, longitude: -48.9566217 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri:
+              'https://avatars2.githubusercontent.com/u/31138844?s=460&u=87f734ea495c79d3db8ffce68263a2b9891ebefc&v=4',
+          }}
+        />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Rafael Borduchi</Text>
+            <Text style={styles.devBio}>Descrição top</Text>
+            <Text style={styles.devTechs}> Node, React, React Native</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
+  },
+  callout: {
+    width: 500,
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+  devTechs: {
+    marginTop: 5,
+  },
+});
